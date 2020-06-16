9972ddddfafd2fb37ced2bbe67a0645a9b2e91f6
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-06-16 21:20:08 +0530

    1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('MY FIRST APP'),
         ),
-        body: Text('welcome'),
+        body: Column(
+          children: <Widget>[
+            Text('The Questions!!!!'),
+            RaisedButton(child: Text('Ans 1'), onPressed: null),
+            RaisedButton(child: Text('Ans 2'), onPressed: null),
+            RaisedButton(child: Text('Ans 3'), onPressed: null),
+          ],
+        ),
       ),
     );
   }
