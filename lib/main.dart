2fbb600b8e5f886f6ef3adaeb30bccbd042240d7
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-06-16 23:17:57 +0530

    button function ok

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,64 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void ansQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    void myQuestionFunction() {}
+
+    var question = [
+      'what is your favorite color',
+      'what is your favorite animal',
+      'what is your favorite car',
+      'what is your favorite bike',
+      'what is your favorite brand',
+      'what is your favorite football player',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.black,
           title: Text('MY FIRST APP'),
         ),
         body: Column(
           children: <Widget>[
-            Text('The Questions!!!!'),
-            RaisedButton(child: Text('Ans 1'), onPressed: null),
-            RaisedButton(child: Text('Ans 2'), onPressed: null),
-            RaisedButton(child: Text('Ans 3'), onPressed: null),
+            Text(question[questionIndex]),
+            RaisedButton(
+              color: Colors.green,
+              child: Text('Ans 1'),
+              onPressed: () {
+                ansQuestion();
+              },
+            ),
+            RaisedButton(
+              color: Colors.red,
+              child: Text('Ans 2'),
+              onPressed: () {
+                ansQuestion();
+              },
+            ),
+            RaisedButton(
+              color: Colors.blue,
+              child: Text('Ans 3'),
+              onPressed: () {
+                ansQuestion();
+              },
+            ),
           ],
         ),
       ),
